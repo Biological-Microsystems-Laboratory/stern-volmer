d651bb3916cfe5b76149946f382cf8b6619658af
Marty <mbrenn3@uic.edu>
2015-06-12 17:01:45 -0500

    deleted files, updated code

--- sternvolmer.m
+++ sternvolmer.m
@@ -1,23 +1,27 @@
-load data/050714/well1.LOG
+load data.csv % data.csv is a single column of intensity data, with the first 4 elements as the two calibration points and corresponding concentration.
 
-z=well1;
+intensity_data=data;
 
-% make a matrix named "z" with calibration intensities and percents for
-% the first 4 elements( I1, %1, I2, %2, exp intensities) 
+I1 = intensity_data(1); % this defines the calibration data based on the element position
+X1 = intensity_data(2); % in the test-data elements 1 and 2 are the zero percent calibration point.
+I2 = intensity_data(3); % elements 3 and 4 are the 21-percent calibration point
+X2 = intensity_data(4);
 
-I1 = z(1);
-I2 = z(3);
-X1 = z(2);
-X2 = z(4);
+%I1 = z(1);
+%I2 = z(3);
+%X1 = z(2);
+%X2 = z(4);
 
 kt = (I2-I1)/((X1*I1)-(X2*I2)); % calculation for Ktau
 I0 = (1+kt*X1)*I1;              % intensity in absence of quencher(0% O2)
 
 x1 = 0:1:100;              % makes x values for plot of stern-volmer
 y1 = I0./(1+kt*x1);        % plots stern-volmer. Check to see if its fitting your calibration points.
-%plot(y1)
-Opercent3 = ((I0./z)-1)/kt;
-%figure
-%plot(Opercent3)
+plot(y1)
 
-save data/050714/well1a.txt Opercent3
+oxygen_percent = ((I0./intensity_data(5:rows(intensity_data),:))-1)/kt;
+
+figure
+plot(oxygen_percent)
+
+%save data/050714/well1a.txt Opercent3
