e1187165e692d4eb8bcb415f31532ccee7ff9b7f
Marty <mbrenn3@uic.edu>
2015-06-18 01:43:30 -0500

    added new equ to correct percent calc

--- sternvolmer.m
+++ sternvolmer.m
@@ -15,11 +15,17 @@ x1 = 0:1:100;              % makes x values for plot of stern-volmer
 intensity_plot = I0./(1+kt*x1);        % plots stern-volmer. Check to see if its fitting your calibration points.
 stern_volmer_plot = x1*kt+1;
 plot(intensity_plot)
+figure
 plot(stern_volmer_plot)
 
 oxygen_percent = ((I0./intensity_data(5:rows(intensity_data),:))-1)/kt;
+K=kt
+m=0.3
+oxygen_percent_corrected = exp((log(I0./intensity_data(5:rows(intensity_data),:))/K)/m)
 figure
 plot(oxygen_percent)
+figure
+plot(oxygen_percent_corrected)
 
 %int= I0./(x1.*kt+1);
 %figure
