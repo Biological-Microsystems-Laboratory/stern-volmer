090f0fd9ebcede46c92853011e978cbf46847503
unknown <mbrenn3@uic.edu>
2015-06-19 15:34:44 -0500

    changed terms to make better sense

--- system_solver.m
+++ system_solver.m
@@ -1,15 +1,17 @@
 clear
 
-syms B D C F x y v u
+syms f1 f2 ksv1 ksv2 x y v u
 
-f1=(B/(1+3*D))+(C/(1+3*F))==5;
-f2=(B/(1+13*D))+(C/(1+13*F))==15; 
-f3=(B/(1+23*D))+(C/(1+23*F))==21; 
-f4=(B/(1+D))+(C/(1+F))==1; 
+%% (f1/(1+ksv1*Q))+(f2/(1+ksv2*Q))== I/I0
+
+eq1=(f1/(1+3*ksv1))+(f2/(1+3*ksv2))==5;
+eq2=(f1/(1+13*ksv1))+(f2/(1+13*ksv2))==15; 
+eq3=(f1/(1+23*ksv1))+(f2/(1+23*ksv2))==21; 
+eq4=(f1/(1+ksv1))+(f2/(1+ksv2))==1; 
 
 %solve([x^2+y == 5, x^2+y^2 == 7],[x, y])
 
-%[solB, solD, solC, solF] = solve([f1, f2, f3, f4], [B, D, C, F])
+%[solf1, solf2, solksv1, solksv2] = solve([eq1, eq2, eq3, eq4], [f1, f2, ksv1, ksv2])
 
 [solv, solu] = solve([2*u^2 + v^2 == 0, u - v == 1], [v, u])
 
