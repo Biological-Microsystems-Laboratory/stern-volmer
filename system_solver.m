7ae6fc6d2011a04c1132bfe4f6d8d9a56a1b4fee
unknown <mbrenn3@uic.edu>
2015-06-19 13:33:46 -0500

    testing system solver

---
+++ system_solver.m
@@ -0,0 +1,20 @@
+clear
+
+syms B D C F x y v u
+
+f1=(B/(1+3*D))+(C/(1+3*F))==5;
+f2=(B/(1+13*D))+(C/(1+13*F))==15; 
+f3=(B/(1+21*D))+(C/(1+21*F))==23; 
+f4=(B/(1+D))+(C/(1+F))==1; 
+
+%solve([x^2+y == 5, x^2+y^2 == 7],[x, y])
+
+%[solB, solD, solC, solF] = solve([f1, f2, f3, f4], [B, D, C, F])
+
+[solv, solu] = solve([2*u^2 + v^2 == 0, u - v == 1], [v, u])
+
+%[y1, y2, y3, y4] = solve(f1,B);
+
+%a = solve (list(f1,f2,f3,f4),list(B,C,D,F));
+
+%subs(f1,list(B,C,D,F),a)
