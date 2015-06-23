322180e0bf5311456483c230d635e7e507619e69
unknown <mbrenn3@uic.edu>
2015-06-23 17:43:25 -0500

    working on fitting

--- sternvolmer.m
+++ sternvolmer.m
@@ -22,19 +22,19 @@ stern_volmer_plot = x1*kt+1;
 intensity_plot = I0./(1+kt*x1); % plots intensity curve.
 subplot (2, 2, 1)
 [hAx,hline1,hline2] = plotyy(x1,intensity_plot,x1,stern_volmer_plot);
-xlabel("% O2")
-ylabel(hAx(1),"intensity") % left axis
-ylabel(hAx(2),"kt*X+1") % right axis
+xlabel('% O2')
+ylabel(hAx(1),'intensity') % left axis
+ylabel(hAx(2),'kt*X+1') % right axis
 
 
 
 
-I = intensity_data(5:rows(intensity_data),:);
+I = intensity_data(5:length(intensity_data),:);
 oxygen_percent = ((I0./I)-1)/kt;
 subplot(2, 2, 3)
 plot(oxygen_percent)
-xlabel("time")
-ylabel("% O2")
+xlabel('time')
+ylabel('% O2')
 
 f1 = 0.25;
 f2 = 0.75;
@@ -59,9 +59,9 @@ corrected_SV_plot = 1./(((f1)./(1+ksv1.*x1))+((f2)./(1+ksv2.*x1)));
 corrected_intensity_plot = I0.*(((f1)./(1+ksv1*x1))+((f2)./(1+ksv2*x1)));
 subplot(2, 2, 2)
 [hBx,hline3,hline4] = plotyy(x1,corrected_intensity_plot,x1,corrected_SV_plot);
-xlabel("% O2")
-ylabel(hBx(1),"intensity") % left axis
-ylabel(hBx(2),"I0*(f1/1+ksv1*x + f1/1+ksv2*x)") % right axis
+xlabel('% O2')
+ylabel(hBx(1),'intensity') % left axis
+ylabel(hBx(2),'I0*(f1/1+ksv1*x + f1/1+ksv2*x)') % right axis
 
 %int= I0./(x1.*kt+1);
 %figure
