aaf753fe29669302f751954d3710b948576a0aa1
unknown <mbrenn3@uic.edu>
2015-06-25 17:05:36 -0500

    working on Q

--- Q.m
+++ Q.m
@@ -3,8 +3,10 @@ clear
 % this code works in octave with the syms package
 
 syms f1 ksv1 Q f2 ksv2 I I0
-symQ = solve((f1/(1+ksv1*Q))+(f2/(1+ksv2*Q))== (I/I0), Q)
-
+%symQ = solve(f1/(1+ksv1*Q)+f2/(1+ksv2*Q)== I/I0, Q)
+symI = solve(f1/(1+ksv1*Q)+f2/(1+ksv2*Q)== I/I0, I)
+%SymQ2 = solve(I0*(f1/(Q*ksv1 + 1) + f2/(Q*ksv2 + 1))==I, Q)
+SymQ3 = solve((I0/Q)*(f1/(ksv1 + 1) + f2/(ksv2 + 1))==I, Q)
 % here is the solution to Q:
 % Qsol = (I*(-I0*f1*ksv2 - I0*f2*ksv1 + I*ksv1 + I*ksv2 - sqrt(4*I*ksv1*ksv2*(I0*f1 + I0*f2 - I) + (I0*f1*ksv2 + I0*f2*ksv1 - I*ksv1 - I*ksv2)^2))/(2*ksv1*ksv2))
 % or
--- Q_test.m
+++ Q_test.m
@@ -1,7 +1,7 @@
 clear
 I0=2376.4;
 I = [ 2376.4; 2148.6; 1000.8; 736.52;];
-f1 = 1;
+f1 = .01;
 f2 = .99;
 ksv1 =  0.002318;
 ksv2 =  0.1436;
@@ -9,9 +9,11 @@ a =(I0^2*f1^2*ksv2^2 + 2*I0^2*f1*f2*ksv1*ksv2 + I0^2*f2^2*ksv1^2 + 2*I0*I*f1*ksv
 b = -I*ksv2 - I*ksv1 + I0*f1*ksv2 + I0*f2*ksv1;
 c = (2*I*ksv1*ksv2);
 Q = ((a.^(1/2))+b)./c;
+QQ = (I0./I).*((f1/ksv1)+(f2/ksv2))
 I = 736.52;
 Q2=((I0^2*f1^2*ksv2^2 + 2*I0^2*f1*f2*ksv1*ksv2 + I0^2*f2^2*ksv1^2 + 2*I0*I*f1*ksv1*ksv2 - 2*I0*I*f1*ksv2^2 - 2*I0*I*f2*ksv1^2 + 2*I0*I*f2*ksv1*ksv2 + I^2*ksv1^2 - 2*I^2*ksv1*ksv2 + I^2*ksv2^2)^(1/2) - I*ksv2 - I*ksv1 + I0*f1*ksv2 + I0*f2*ksv1)/(2*I*ksv1*ksv2);
 Q3=-(I*ksv1 + I*ksv2 + (I0^2*f1^2*ksv2^2 + 2*I0^2*f1*f2*ksv1*ksv2 + I0^2*f2^2*ksv1^2 + 2*I0*I*f1*ksv1*ksv2 - 2*I0*I*f1*ksv2^2 - 2*I0*I*f2*ksv1^2 + 2*I0*I*f2*ksv1*ksv2 + I^2*ksv1^2 - 2*I^2*ksv1*ksv2 + I^2*ksv2^2)^(1/2) - I0*f1*ksv2 - I0*f2*ksv1)/(2*I*ksv1*ksv2);
+Q4=(I0*(f1/(ksv1 + 1) + f2/(ksv2 + 1)))/I;
 % a = [1:1:4];
 % b = [2:1:5];
 % c = [3:1:6];
@@ -23,3 +25,4 @@ Q3=-(I*ksv1 + I*ksv2 + (I0^2*f1^2*ksv2^2 + 2*I0^2*f1*f2*ksv1*ksv2 + I0^2*f2^2*ks
 Q
 Q2
 Q3
+Q4
