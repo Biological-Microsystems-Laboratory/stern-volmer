368456a9f804e205e73aecb8dd61101833d152b2
unknown <mbrenn3@uic.edu>
2015-06-29 13:00:09 -0500

    fixed fitting equation

--- fitting_two_site.m
+++ fitting_two_site.m
@@ -1,9 +1,9 @@
 Q = [ 0; 1; 10; 21;];
 I = [ 2376.4; 2148.6; 1000.8; 736.52;];
 % (f1/(1+ksv1*Q))+(1-f1/(1+ksv2*Q))== I/I0 ; f1+f2=1, f1=1-f2 f2=1-f1
-g = fittype('2376.4*((1-f2/(1+ksv1*Q))+(f2/(1+ksv2*Q)))',...
-            'independent',{'Q'},...
-            'problem',{'f2'}';
-myfit = fit(Q,I,g,'problem',{.99})
+g = fittype('2376.4*(((1-f2)/(1+ksv1*Q))+(f2/(1+ksv2*Q)))',...
+            'independent',{'Q'},'dependent','I',...
+            'problem',{'f2'}')
+myfit = fit(Q,I,g,'problem',{.45})
 figure
 plot(myfit,Q,I)
