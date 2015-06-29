6ab02192bad6b227b33031dc5a04f65945fdea51
unknown <mbrenn3@uic.edu>
2015-06-29 16:28:05 -0500

    used range on coeff

--- fitting_two_site.m
+++ fitting_two_site.m
@@ -1,9 +1,10 @@
-Q = [ 0; 1; 10; 21;];
+Q = [ 0; .01; .1; .21;];
 I = [ 2376.4; 2148.6; 1000.8; 736.52;];
 % (f1/(1+ksv1*Q))+(1-f1/(1+ksv2*Q))== I/I0 ; f1+f2=1, f1=1-f2 f2=1-f1
-g = fittype('2376.4*(((1-f2)/(1+ksv1*Q))+(f2/(1+ksv2*Q)))',...
-            'independent',{'Q'},'dependent','I',...
-            'problem',{'f2'}')
-myfit = fit(Q,I,g,'problem',{.45})
+g = fittype('2376.4*((f1/(1+ksv1*Q))+((1-f1)/(1+ksv2*Q)))',...
+            'independent',{'Q'},'dependent','I')
+fa = .2
+fb = 1-fa
+myfit = fit(Q,I,g,'lower',[0 0 0],'upper',[1 inf inf])
 figure
 plot(myfit,Q,I)
