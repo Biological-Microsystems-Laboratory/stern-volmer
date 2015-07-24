262103c8bb028cc3657c263e81d587e751e9f3ec
mbrenn3 <mbrenn3@uic.edu>
2015-07-24 14:11:34 -0500

    two-site fitting for 24 wells

--- fitting_two_site.m
+++ fitting_two_site.m
@@ -3,8 +3,7 @@ I = [ 2376.4; 2148.6; 1000.8; 736.52;];
 % (f1/(1+ksv1*Q))+(1-f1/(1+ksv2*Q))== I/I0 ; f1+f2=1, f1=1-f2 f2=1-f1
 g = fittype('2376.4*((f1/(1+ksv1*Q))+((1-f1)/(1+ksv2*Q)))',...
             'independent',{'Q'},'dependent','I')
-fa = .2
-fb = 1-fa
 myfit = fit(Q,I,g,'lower',[0 0 0],'upper',[1 inf inf])
 figure
 plot(myfit,Q,I)
+c = coeffvalues(myfit) % reads out coefficent values
--- results_import.m
+++ results_import.m
@@ -45,3 +45,14 @@ hold on
 %    plot(oxygen_percent(:,i))
 %end
 hold off
+
+%%% Two-Site Stern-Volmer fitting
+Q = calibration_tanks';
+for i = 1:24
+    I = calibration(:,i);
+    % (f1/(1+ksv1*Q))+(1-f1/(1+ksv2*Q))== I/I0 ; f1+f2=1, f1=1-f2 f2=1-f1
+    g = fittype('2376.4*((f1/(1+ksv1*Q))+((1-f1)/(1+ksv2*Q)))',...
+            'independent',{'Q'},'dependent','I')
+    myfit = fit(Q,I,g,'lower',[0 0 0],'upper',[1 inf inf]);
+    coeff_twosite(:,i) = coeffvalues(myfit)';
+end
