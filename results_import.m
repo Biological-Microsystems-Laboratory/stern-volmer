fe562f3cc7ff2656760b6c791c4062ec89c6a140
mbrenn3 <mbrenn3@uic.edu>
2015-07-24 13:38:54 -0500

    have two point stern working for 24 wells

--- results_import.m
+++ results_import.m
@@ -23,3 +23,25 @@ end
 
 %csvwrite('calibration.csv',calibration)
 exp = wells(number_of_calibration_points*exposures_per_calibration+1:length(wells),:);
+
+%%% Stern-Volmer
+calibration_tanks = [0, 1, 7.5, 15, 21];
+I1 = calibration(1,:);
+I2 = calibration(number_of_calibration_points,:);
+X1 = calibration_tanks(1);
+X2 = calibration_tanks(number_of_calibration_points);
+kt = (I2-I1)./((X1*I1)-(X2*I2)); % calculation for Ktau
+I0 = I1;
+x1 = [0:1:25]'; % makes x values for plot of stern-volmer
+%stern_volmer_plot = x1*kt+1; % plots I0/I Vs oxygen
+%intensity_plot = I0./(1+kt*x1); % plots intensity(I) Vs oxygen
+
+for i = 1:24
+    oxygen_percent(:,i) = ((I0(i)./exp(:,i))-1)/kt(i);
+end
+
+hold on
+%for i= 1:24
+%    plot(oxygen_percent(:,i))
+%end
+hold off
