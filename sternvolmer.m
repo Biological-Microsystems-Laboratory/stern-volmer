c73c9caa83e7fc74091ad196482a31cf35f47172
mbrenn3 <mbrenn3@uic.edu>
2015-08-23 21:47:44 -0500

    overwrote to new sternvolmer.m updated readme with new plot

--- sternvolmer.m
+++ sternvolmer.m
@@ -1,83 +1,89 @@
 clear
-load data.csv % data.csv is a single column of intensity data, with the first 4 elements as the two calibration points and corresponding concentration.
-load calibration.csv %
-calibration_tanks = [0, 1, 7.5, 15, 21]; %
-intensity_data=data; % enters loaded data into new matrix
+load timecourse_intensities.csv 
+load calibration_intensities.csv
+calibration_tanks = [0, 1, 7.5, 15, 21];
 
-I1 = intensity_data(1); % this defines the calibration data based on the element position.
-X1 = intensity_data(2); % in the test-data elements 1 and 2 are the zero percent calibration point.
-I2 = intensity_data(3); % elements 3 and 4 are the 21-percent calibration point.
-X2 = intensity_data(4);
+%%%%% Calculation for ksv
+I0 = calibration_intensities(1); % intensity in absence of quencher
+Ic = calibration_intensities(5);
+Q0 = calibration_tanks(1);
+Qc = calibration_tanks(5);
+ksv = (Ic-I0)/((Q0*I0)-(Qc*Ic));
 
 
-kt = (I2-I1)/((X1*I1)-(X2*I2)); % calculation for Ktau
-I0 = (1+kt*X1)*I1;              % intensity in absence of quencher(0% O2)
-
-x1 = 0:1:25; % makes x values for plot of stern-volmer
-
-stern_volmer_plot = x1*kt+1; % plots I0/I Vs oxygen
-intensity_plot = I0./(1+kt*x1); % plots intensity(I) Vs oxygen
-
-I = intensity_data(5:length(intensity_data),:); % makes new matrix with only intensity data
-oxygen_percent = ((I0./I)-1)/kt;
+oxygen_percent_standard = ((I0./timecourse_intensities)-1)/ksv;
 
 % plotting for standard S-V
-subplot (2, 1, 1)
-[hAx,hline1,hline2] = plotyy(x1,intensity_plot,x1,stern_volmer_plot);
-hline1.Color = 'r';
-hline2.Color = 'b';
-title('Standard Stern-Volmer and Intensity Plot')
-xlabel('% O2')
-ylabel(hAx(1),'intensity') % left axis
-ylabel(hAx(2),'kt*X+1') % right axis
+%subplot (2, 1, 1)
+%[hAx,hline1,hline2] = plotyy(x1,intensity_plot,x1,stern_volmer_plot);
+%hline1.Color = 'r';
+%hline2.Color = 'b';
+%title('Standard Stern-Volmer and Intensity Plot')
+%xlabel('% O2')
+%ylabel(hAx(1),'intensity') % left axis
+%ylabel(hAx(2),'kt*X+1') % right axis
 
 % plotting oxygen percent
-subplot(2, 1, 2)
-plot(oxygen_percent)
-title('Oxygen for Standard Stern-Volmer')
+%subplot(2, 1, 2)
+%plot(oxygen_percent_standard)
+%title('Oxygen for Standard Stern-Volmer')
+%xlabel('time')
+%ylabel('% O2')
 
-xlabel('time')
-ylabel('% O2')
+%%% Two-Site Stern-Volmer fitting
+Q = calibration_tanks';
+I = calibration_intensities;
+% This is the two-site model: (f1/(1+ksv1*Q))+(1-f1/(1+ksv2*Q))== I/I0 ; f1+f2=1, f1=1-f2 f2=1-f1
+g = fittype('I0*((f1/(1+ksv1*Q))+((1-f1)/(1+ksv2*Q)))',...
+            'independent',{'Q'},'dependent','I','problem','I0');
+myfit = fit(Q,I,g,'problem',I0,'lower',[0 0 0],'upper',[1 inf inf],'Start',[0, 0, 0]);
+coeff_twosite = coeffvalues(myfit)';
+%hold on
+%figure   
+%plot(myfit,Q,I) % plots the fitting
 
-
-%%%%%%%%%%%%%%%%%%%% TWO-SITE MODEL
-
-f1 =   0.07546;
+%%%%% Calculation of oxygen from two site     
+f1 = coeff_twosite(1);
 f2 = 1-f1;
-ksv1 = 3.54e-10;
-ksv2 = 0.1513;
-
-% Solving for Q
+ksv1 = coeff_twosite(2);
+ksv2 = coeff_twosite(3);
+I = timecourse_intensities;
 a =(I0^2*f1^2*ksv2^2 + 2*I0^2*f1*f2*ksv1*ksv2 + I0^2*f2^2*ksv1^2 + 2*I0*I*f1*ksv1*ksv2 - 2*I0*I*f1*ksv2^2 - 2*I0*I*f2*ksv1^2 + 2*I0*I*f2*ksv1*ksv2 + I.^2*ksv1^2 - 2*I.^2*ksv1*ksv2 + I.^2*ksv2^2);
 b = -I*ksv2 - I*ksv1 + I0*f1*ksv2 + I0*f2*ksv1;
 c = (2*I*ksv1*ksv2);
-Q = ((a.^(1/2))+b)./c;
+oxygen_percent_twosite = ((a.^(1/2))+b)./c;
+%figure
+%plot(oxygen_percent_twosite)
 
-% plots with two-site model added
+%%%% plotting
+
+x = 0:1:21; % makes x values for plot of stern-volmer
+stern_volmer_plot = (x*ksv+1)'; % values for I0/I Vs oxygen
+intensity_plot = (I0./(1+ksv*x))'; % plots intensity(I) Vs oxygen
+corrected_SV_plot = 1./(((f1)./(1+ksv1.*x))+((f2)./(1+ksv2.*x)))'; 
+corrected_intensity_plot = I0.*(((f1)./(1+ksv1*x))+((f2)./(1+ksv2*x)))';
 figure
-corrected_SV_plot = 1./(((f1)./(1+ksv1.*x1))+((f2)./(1+ksv2.*x1))); 
-corrected_intensity_plot = I0.*(((f1)./(1+ksv1*x1))+((f2)./(1+ksv2*x1)));
 subplot(2, 1, 1)
-[hBx,hline3,hline4] = plotyy(x1,corrected_intensity_plot,x1,corrected_SV_plot);
-hline3.LineStyle = ':';
-hline4.LineStyle = ':';
-title('Stern-Volmer Plot from Two-Site Model')
+[AX,H1,H2] = plotyy(x,[corrected_intensity_plot,intensity_plot],x,[corrected_SV_plot,stern_volmer_plot]);
+legend([H1;H2],'two-site int','standard int','two-site SV','standard SV');
+LineArray = {'LineStyle'};
+ValueLine = {'-','--'}';
+ColorArray = {'Color'};
+ValueColor = {'b','r'}';
+ValueColor2 = {'r','b'}';
+set(H1,LineArray,ValueLine,ColorArray,ValueColor)
+set(H2,LineArray,ValueLine,ColorArray,ValueColor2)
+title('Stern-Volmer Standard Vs. Two-Site Model')
 xlabel('% O2')
-ylabel(hBx(1),'intensity') % left axis
-ylabel(hBx(2),'I0*(f1/1+ksv1*x + f1/1+ksv2*x)') % right axis
-hold on
-[hBx,hline3,hline4] = plotyy(x1,intensity_plot,x1,stern_volmer_plot);
-hold off
+ylabel(AX(1),'intensity') % left axis
+ylabel(AX(2),'I0/I') % right axis
 
+% plot timecourse data
 subplot(2, 1, 2)
-plot(Q)
+twosite = plot(oxygen_percent_twosite,'g');
 hold on
-plot(oxygen_percent)
-title('Oxygen Two-Site Stern-Volmer')
+standard = plot(oxygen_percent_standard,'m');
+legend([twosite;standard],'two-site','standard');
+title('Analyzed Time Course Data with 10% Oxygen Applied')
 xlabel('time')
 ylabel('% O2')
-%int= I0./(x1.*kt+1);
-%figure
-%plot(int)
-
-%save percent.csv oxygen_percent % optional line for saving analyzed data to a file
